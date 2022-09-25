18f02c32db209abf17611022821b3a1c4a46e65a
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2022-09-26 01:16:24 +0600

    Add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './components/Home';
+import Aboutus from './components/Aboutus';
+import Service from './components/Service';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li> <Link to="/">Home</Link> {" "}</li>
+        <li> <Link to="about">About</Link></li>
+        <li><Link to="service">Service</Link></li>
+        <li><Link to="contact">Contact</Link></li>
+      </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<Aboutus />} />
+      <Route path="/service" element={<Service />} />
+      <Route path="/contact" element={<Contact />} />
+   
+    </Routes>
+    
+    </>
   );
 }
 
