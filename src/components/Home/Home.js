481d5bda80cbe763763ba00bba271a0228b00eed
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2022-09-27 01:24:38 +0600

    Add all sliding images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import './Home.css';
 import {sliderData} from './Slider-data.js';
@@ -6,16 +6,46 @@ import {sliderData} from './Slider-data.js';
 
 const Home = () => {
   const[currentSlide, setCurrentSlide] = useState(0);
+  const slideLength = sliderData.length;
+
+  const autoScroll = true;
+  let slideInterval;
+  let intervalTime = 4000;
+
+  function auto () {
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+
+  useEffect (() => {
+    if (autoScroll) {
+        auto()
+    }
+    return () => clearInterval(slideInterval)
+  },[currentSlide])
+
+  useEffect (() => {
+    auto ()
+  },[])
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+      console.log("slide2");
+  }
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+      console.log("slide1");
+  }
+
   return (
     <div className="slider">
-      <AiOutlineArrowLeft className="arrow prev" />
-      <AiOutlineArrowRight className="arrow next" />
+      <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide}/>
+      <AiOutlineArrowRight className="arrow next" onClick={nextSlide}/>
 
       {sliderData.map((slide, index) => {
         return(
           <div className={index === currentSlide ? "slide current" : "slide"}
           key={index}>
-            {index=== currentSlide && (
+            {index === currentSlide && (
               <div>
                 <img src={slide.image} alt="slide-image" className="image"/>
                 <div className="content">
