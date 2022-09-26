f229b3765c6a06f103e55ee5811b3166673c9eeb
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2022-09-26 21:05:19 +0600

    Add images in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import './Home.css';
+import {sliderData} from './Slider-data.js';
+
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
+  const[currentSlide, setCurrentSlide] = useState(0);
+  return (
+    <div className="slider">
+      <AiOutlineArrowLeft className="arrow prev" />
+      <AiOutlineArrowRight className="arrow next" />
+
+      {sliderData.map((slide, index) => {
+        return(
+          <div className={index === currentSlide ? "slide current" : "slide"}
+          key={index}>
+            {index=== currentSlide && (
+              <div>
+                <img src={slide.image} alt="slide-image" className="image"/>
+                <div className="content">
+                  <h2>{slide.heading}</h2>
+                  <p>{slide.desc}</p>
+                  <hr/>
+                  <button className="btn">Contact Us</button>
+                </div>
+              </div>
+              
+            )}
+
+          </div>
+          )
+      })}
+    </div>
+  )
 }
 
 export default Home
